73cd45433ee27cb5ccdd2d88d34e3376c5803147
Bikoda <juanfranciscomaggi@gmail.com>
2016-03-16 12:26:43 -0300

    Update and rename javascript to javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+var nombre;
+
+nombre = prompt ("ingresa tu nombre");
+
+if (nombre == "Bikoda") {
+	alert(" Bienvenido Sir Bikoda")
+} else if (nombre == "benjamin"){
+	alert("Hola gordon!")
+} else {
+	alert("Bienvenido")
+}
+
