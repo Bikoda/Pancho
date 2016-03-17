1fee3cc88df9d7b8f7819ace555d6864884e84db
Bikoda <juanfranciscomaggi@gmail.com>
2016-03-17 01:27:23 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -6,12 +6,6 @@ if (nombre == "Bikoda") {
 	alert(" Bienvenido Sir Bikoda")
 } else if (nombre == "benjamin"){
 	alert("Hola gordon!")
-} else if (nombre == "david"){
-	alert("Sup ladiesman")
-} else if (nombre == "mario"){
-	alert("Hola primaso!")
-} else if (nombre == "sol"){
-	alert("Hi swettie!")
-} else	alert("Bienvenido")
+} else	alert("Bienvenido"){
 }
 
