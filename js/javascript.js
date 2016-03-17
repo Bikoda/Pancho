bacf0c3887800b9582c36202d3d5f74cdcca4a4d
Bikoda <juanfranciscomaggi@gmail.com>
2016-03-17 01:31:47 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -6,6 +6,8 @@ if (nombre == "bikoda") {
 	document.write ("Buenos dias Bikoda")
 } else if (nombre == "benja"){
 	document.write ("Hola Gordon!")
+} else if (nombre == "ciro"){
+	document.write ("sup nebu"){
 } else {
 	document.write ("bienvenidos a Bikoda")
 }
