3721697630072c23811f0c1778aabdc05a001b35
Bikoda <juanfranciscomaggi@gmail.com>
2016-03-17 11:28:38 -0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,5 @@
-var nombre;
 var i = 0;
 
-nombre = prompt ("ingresa tu nombre");
-
-if (nombre == "bikoda") {
-	alert("Buenos dias Bikoda");
-} else if (nombre == "benja") {
-	alert("Hola Gordon!");
-} else if (nombre == "asd") {
-	while (i <= 1000000){
-		alert("Access Denied!");
-} else {
-	alert("bienvenidos a Bikoda");
+while (i <= 20){
+ console.log("You have been Hacked!");
 }
