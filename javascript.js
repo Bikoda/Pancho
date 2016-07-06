c65ea731641eac3160c25c805a90a583627d9a17
Bikoda <juanfranciscomaggi@gmail.com>
2016-07-06 18:28:53 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 function option1() {
-alert('option 1');
-}
+        var name = prompt('Account','Poppy');
+        if (name != null && name != "") {
+            alert(name);
+        }
+    }
 
 function option11(){
 document.getElementById('verde').style.display = 'block';
@@ -30,6 +33,27 @@ document.getElementById('rosa').style.display = 'none';
 document.getElementById('verde').style.display = 'none';
 }
 
+function option2(){
+	alert(name);
+}
 
+function optioncajas(){
+document.getElementById('cajas').style.display = 'block';
+document.getElementById('triangles').style.display = 'none';
 
+}
+
+function optiontriangles(){
+document.getElementById('Triangles').style.display = 'block';
+document.getElementById('cajas').style.display = 'none';
+
+}
+function option55(){
+document.getElementById('red').style.display = 'block';
+document.getElementById('yellow').style.display = 'none';
+}
+function option66(){
+document.getElementById('yellow').style.display = 'block';
+document.getElementById('red').style.display = 'none';
+}
 
