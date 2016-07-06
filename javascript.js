715c7eaf02a4a40ef131600153086b48fbd18fc5
Bikoda <juanfranciscomaggi@gmail.com>
2016-07-06 16:15:17 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function option1() {
+alert('option 1');
+}
+
+function option11(){
+document.getElementById('verde').style.display = 'block';
+document.getElementById('azul').style.display = 'none';
+document.getElementById('rosa').style.display = 'none';
+document.getElementById('marron').style.display = 'none';
+}
+
+function option22(){
+document.getElementById('azul').style.display = 'block';	
+document.getElementById('verde').style.display = 'none';
+document.getElementById('rosa').style.display = 'none';
+document.getElementById('marron').style.display = 'none';
+}
+
+function option33(){
+document.getElementById('rosa').style.display = 'block';
+document.getElementById('azul').style.display = 'none';
+document.getElementById('azul').style.display = 'none';
+document.getElementById('marron').style.display = 'none';
+}
+
+function option44(){
+document.getElementById('marron').style.display = 'block';
+document.getElementById('azul').style.display = 'none';
+document.getElementById('rosa').style.display = 'none';
+document.getElementById('verde').style.display = 'none';
+}
+
+
+
+
