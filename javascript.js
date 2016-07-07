fd74bb5225450ea20481a9ea08e93284a933feda
Bikoda <juanfranciscomaggi@gmail.com>
2016-07-07 19:59:39 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,3 @@
-function option1() {
-        var name = prompt('Account','Poppy');
-        if (name != null && name != "") {
-            alert(name);
-        }
-    }
-
 function option11(){
 document.getElementById('verde').style.display = 'block';
 document.getElementById('azul').style.display = 'none';
@@ -36,15 +29,30 @@ document.getElementById('verde').style.display = 'none';
 function option2(){
 	alert(name);
 }
-
+function singup1(){
+document.getElementById('singup').style.display = 'block';
+document.getElementById('cajas').style.display = 'none';
+document.getElementById('triangles').style.display = 'none';
+document.getElementById('login').style.display = 'none';
+}
+function login1(){
+document.getElementById('login').style.display = 'block';
+document.getElementById('triangles').style.display = 'none';
+document.getElementById('singup').style.display = 'none';
+document.getElementById('cajas').style.display = 'none';
+}
 function cajas1(){
 document.getElementById('cajas').style.display = 'block';
 document.getElementById('triangles').style.display = 'none';
+document.getElementById('singup').style.display = 'none';
+document.getElementById('login').style.display = 'none';
 }
 
 function triangles1(){
 document.getElementById('triangles').style.display = 'block';
 document.getElementById('cajas').style.display = 'none';
+document.getElementById('singup').style.display = 'none';
+document.getElementById('login').style.display = 'none';
 }
 
 function option55(){
@@ -55,4 +63,8 @@ document.getElementById('yellow').style.display = 'none';
 function option66(){
 document.getElementById('yellow').style.display = 'block';
 document.getElementById('red').style.display = 'none';
+}
+
+function optionw(){
+	window.open("https://community.box.com/");
 }
