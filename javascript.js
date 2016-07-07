4f7b262ec7aed1c353486bc0adb6466aef2a7406
Bikoda <juanfranciscomaggi@gmail.com>
2016-07-07 20:08:59 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -68,3 +68,6 @@ document.getElementById('red').style.display = 'none';
 function optionw(){
 	window.open("https://community.box.com/");
 }
+function oprionm(){
+	window.open("www.junctiontriangle.ca/forum");
+}
